1d4dd2d364da24f9d106ccd37202c59e0813fc74
Calteo <matthias@famger.de>
2020-01-23 20:44:30 +0100

    automated documentation and publishing

--- docfx_project/index.md
+++ docfx_project/index.md
@@ -1,4 +1,4 @@
-# This is the **HOMEPAGE**.
-Refer to [Markdown](http://daringfireball.net/projects/markdown/) for how to write markdown files.
-## Quick Start Notes:
-1. Add images to the *images* folder if the file is referencing an image.
+# Toolbox.Xml.Serialization
+
+This is a small compoment for convering an object to xml and back.
+
---
+++ uploadDocs.ps1
@@ -0,0 +1,47 @@
+#
+# uploads a documetation folder to the gh_branch
+#
+[CmdletBinding()]
+Param
+  (    
+    [parameter(Mandatory=$true,Position=1)]
+    [String]$SiteFolder,
+    [parameter(Mandatory=$true,Position=2)]
+    [String]$Project,
+	[parameter(Mandatory=$true,Position=3)]
+    [String]$User,
+	[parameter(Mandatory=$true,Position=4)]
+    [String]$EMail,	
+	[parameter(Mandatory=$true,Position=5)]
+    [String]$Token
+  )
+
+echo "init git"
+git config --global credential.helper store
+Add-Content "$HOME\.git-credentials" "https://$Token:x-oauth-basic@github.com`n"
+git config --global user.email $EMail
+git config --global user.name $User
+
+$source=$pwd
+$repro="$pwd\..\$Project-Documentation"
+
+echo "removing temporary doc directory $repro"
+rm $repro -Force -Recurse -ErrorAction SilentlyContinue
+mkdir $repro | Out-Null
+
+echo "cloning the repo with the gh-pages branch"
+git clone "https://github.com/$User/$Project.git" --branch gh-pages $repro
+
+echo "clear repo directory"
+cd $repro
+git rm -r *
+
+echo "copy documentation into the repo"
+cp -r "$source\$SiteFolder\*" .
+
+echo "push the new docs to the gh-pages branch"
+git add . -A
+git commit -m "update generated documentation"
+git push origin gh-pages
+
+cd $source
